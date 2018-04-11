67cb7b8976262cbf02dc454456105ba3e85de236
Bijoy Kar <38171544+freelancer-bijoy@users.noreply.github.com>
2018-04-11 19:30:22 +0600

    Update register-nav-menus.php

--- register-nav-menus.php
+++ register-nav-menus.php
@@ -1,4 +1,5 @@
 <?php 
+	//Register Menus
 	register_nav_menus( array(
 		'navigation_menu' => 'Theme Navigation Menu',
 		'footer_menu' => 'My Custom Footer Menu',
@@ -6,5 +7,13 @@
 
 ?>
 
-// Display Menus on Theme
-<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+
+<?php 
+	// Display Menus on Theme	
+	wp_nav_menu( array( 
+		'theme_location' => 'navigation_menu',
+		'container'      => 'div',
+		'container_class'      => 'navbar-collapse collapse align-left',
+		'menu_class'           => 'nav navbar-nav',
+	)); 
+?>
