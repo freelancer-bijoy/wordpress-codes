01a3d4222b9e60a6f9a569a0d42db810a92615f1
Bijoy Kar <38171544+freelancer-bijoy@users.noreply.github.com>
2018-04-12 13:16:51 +0600

    Update custom-widgets.php

--- custom-widgets.php
+++ custom-widgets.php
@@ -1,44 +1,68 @@
-class My_Widget extends WP_Widget {
+/**
+ * Adds Foo_Widget widget.
+ */
+class Foo_Widget extends WP_Widget {
 
 	/**
-	 * Sets up the widgets name etc
+	 * Register widget with WordPress.
 	 */
-	public function __construct() {
-		$widget_ops = array( 
-			'classname' => 'my_widget',
-			'description' => 'My Widget is awesome',
+	function __construct() {
+		parent::__construct(
+			'foo_widget', // Base ID
+			esc_html__( 'Widget Title', 'text_domain' ), // Name
+			array( 'description' => esc_html__( 'A Foo Widget', 'text_domain' ), ) // Args
 		);
-		parent::__construct( 'my_widget', 'My Widget', $widget_ops );
 	}
 
 	/**
-	 * Outputs the content of the widget
+	 * Front-end display of widget.
 	 *
-	 * @param array $args
-	 * @param array $instance
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
 	 */
 	public function widget( $args, $instance ) {
-		// outputs the content of the widget
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
+		}
+		echo esc_html__( 'Hello, World!', 'text_domain' );
+		echo $args['after_widget'];
 	}
 
 	/**
-	 * Outputs the options form on admin
+	 * Back-end widget form.
 	 *
-	 * @param array $instance The widget options
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
 	 */
 	public function form( $instance ) {
-		// outputs the options form on admin
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'New title', 'text_domain' );
+		?>
+		<p>
+		<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'text_domain' ); ?></label> 
+		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php 
 	}
 
 	/**
-	 * Processing widget options on save
+	 * Sanitize widget form values as they are saved.
 	 *
-	 * @param array $new_instance The new options
-	 * @param array $old_instance The previous options
+	 * @see WP_Widget::update()
 	 *
-	 * @return array
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
 	 */
 	public function update( $new_instance, $old_instance ) {
-		// processes widget options to be saved
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+		return $instance;
 	}
-}
+
+} // class Foo_Widget
