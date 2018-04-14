f3c199684760a7f95770dc006befbf06a0dedfc4
Bijoy Kar <38171544+freelancer-bijoy@users.noreply.github.com>
2018-04-14 10:54:12 +0600

    Update shortcodes.php

--- shortcodes.php
+++ shortcodes.php
@@ -17,4 +17,15 @@
 
 
 ////////////////Second Style/////////////
-
+<?php
+	function themeShortcodes($atts, $content)
+	{
+		extract(shortcode_atts( array(
+			'color' => 'black', 
+			'font_size'  => '25px', 
+		), $atts ));
+		return "<p style='color: ".$color."; font-size: ".$font_size.";'>".$content."</p>";
+	}
+	add_shortcode( 'hello', 'themeShortcodes' );
+?>
+[hello color= "blue" font-size= "30px"]Hello This Bijoy[/hello]
